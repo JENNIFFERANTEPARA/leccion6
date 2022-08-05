43adaa3d2aaa7439652f4ddb82c27eac0d86ff39
jenniffer <prueba2022jenniffer@hotmail.com>
2022-08-05 01:24:29 -0500

    ejercicio 1 ej

---
+++ leccion1.js
@@ -0,0 +1,9 @@
+<script>
+
+    let nombre = prompt('Ingrese su nombre completo');
+    let contador_palabras = nombre.split(' ');
+    console.log(contador_palabras);
+
+    console.log(`Hola mi nombre es: ` + nombre + ` este es mi primer scrip.`);
+
+</script>
