26b3b4562cff331245baa19e8d2345c48173b520
JENNIFFERANTEPARA <108249866+JENNIFFERANTEPARA@users.noreply.github.com>
2022-08-05 11:48:38 -0500

    Update leccion1.js

--- leccion1.js
+++ leccion1.js
@@ -1,3 +1,14 @@
+ /*Lección 1: Introducción a Lenguaje Formal
+Ejercicio
+● Crear un archivo html
+● Crear un archivo js, que se guarde en la misma carpeta que el
+archivo html
+● En el archivo js, escribe las instrucciones necesarias para que se
+muestre “Hola mi nombre es…tu nombre…este es mi primer
+script”
+● En el archivo html, vincula en archivo js para que este se ejecute
+  
+    **/
 <script>
 
     let nombre = prompt('Ingrese su nombre completo');
